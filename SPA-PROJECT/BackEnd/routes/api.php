07478f37f739280433ca7042053efd43af597e15
Yeni <1609yalmada.villalba@gmai.com>
2026-04-14 23:18:12 -0300

    modificacion de servicio

--- SPA-PROJECT/BackEnd/routes/api.php
+++ SPA-PROJECT/BackEnd/routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\ServicioController;
 
 Route::apiResource('usuarios', UsuarioController::class);
+Route::apiResource('servicios', ServicioController::class);
